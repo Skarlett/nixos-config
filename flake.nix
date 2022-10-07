7b53f18a2933ca93e092594224bc2a411d067577
skarlett <skarlett@github.com>
2022-10-07 10:09:01 -0500

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    coggiebot.url ="github:skarlett/coggie-bot";
   };
 
   outputs = { nixpkgs, ... }: {
