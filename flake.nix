f18f51d646e8c41e8930acfb7df209382157ca0a
Skarlett <Skarlett@github.com>
2023-05-26 21:44:41 -0500

    add system pin

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
       system = "x86_64-linux";
       sshkey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILcon6Pn5nLNXEuLH22ooNR97ve290d2tMNjpM8cTm2r lunarix@masterbook";
       pkgs = import nixpkgs { inherit system; };
+      system-nix-path = ;
     in {
       nixosConfigurations.flagship =
         nixpkgs.lib.nixosSystem
@@ -32,10 +33,11 @@
             specialArgs = { inherit self; };
             modules = [
               ./flagship.nix
+	      
               nur.nixosModules.nur
-
               ({lib, config, ...}: {
                 networking.hostName = "flagship";
+                nix.registry.nixpkgs.flake = nixpkgs;
                 nixpkgs.config.allowUnfree = true;
                 nixpkgs.overlays = [
                   (final: prev: {
