59c5a8b52629cd5b7b83e6be2ff8f72b35d2aae4
Skarlett <Skarlett@github.com>
2023-05-26 21:45:32 -0500

    fix: add system pin

--- flake.nix
+++ flake.nix
@@ -24,7 +24,6 @@
       system = "x86_64-linux";
       sshkey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILcon6Pn5nLNXEuLH22ooNR97ve290d2tMNjpM8cTm2r lunarix@masterbook";
       pkgs = import nixpkgs { inherit system; };
-      system-nix-path = ;
     in {
       nixosConfigurations.flagship =
         nixpkgs.lib.nixosSystem
