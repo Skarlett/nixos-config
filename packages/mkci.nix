3eb36322de376b4c97ca8e43d39835535294ab6e
Skarlett <Skarlett@github.com>
2023-06-09 08:45:16 -0500

    i fk h8 yaml

--- packages/mkci.nix
+++ packages/mkci.nix
@@ -12,15 +12,15 @@
           pull_request:
             branches: [ "master" ]
         jobs:
-            ${name}:
-                runs-on: ubuntu-latest
-                steps:
-                - uses: actions/checkout@v3
-                - uses: cachix/install-nix-action@v20
-                with:
-                    nix_path: nixpkgs=channel:nixos-unstable
-                    github_access_token: \''${{ secrets.GITHUB_TOKEN }}
-                - run: ${cmd}
+          ${name}:
+            runs-on: ubuntu-latest
+            steps:
+            - uses: actions/checkout@v3
+            - uses: cachix/install-nix-action@v20
+              with:
+                nix_path: nixpkgs=channel:nixos-unstable
+                github_access_token: \''${{ secrets.GITHUB_TOKEN }}
+            - run: ${cmd}
         EOF
       '';
 
