454704d01eb4e6784c923ba2dd4909892a102339
Skarlett <Skarlett@github.com>
2023-06-09 04:05:28 -0500

    accessible.nix: add ssh enable and trust store

--- machines/coggie.nix
+++ machines/coggie.nix
@@ -1,8 +1,6 @@
 { keys, pkgs, config, lib, ... }: 
 {
-  documentation.enable = false;
-  
-  system.stateVersion = "22.11";  
+  system.stateVersion = "22.11";
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
   services.journald.extraConfig = ''
@@ -11,18 +9,11 @@
 
   hardware.enableRedistributableFirmware = true;
   networking.wireless.enable = true;
-  services.openssh.enable = true;
   networking.firewall = {
     allowedUDPPorts = [ 22 ];
     allowedTCPPorts = [ 22 ];
   };
 
-  nix.settings.trusted-public-keys = [
-    keys.flagship.store
-  ]; 
-
-
-
-
+  documentation.enable = false;
   documentation.man.generateCaches = lib.mkForce false;
 }
--- modules/accessible.nix
+++ modules/accessible.nix
@@ -1,4 +1,11 @@
 {config, lib, pkgs, keys, ...}:
 {
+    services.openssh.enable = true;
+
     users.users.lunarix.openssh.authorizedKeys.keys = [ keys.flagship.lunarix ];
+    users.users.root.openssh.authorizedKeys.keys = [ keys.flagship.lunarix ];
+
+    nix.settings.trusted-public-keys = [
+        keys.flagship.store
+    ];
 }
