460b8e5d728649c8ba41b0041d876e620e242998
Skarlett <Skarlett@github.com>
2023-07-12 22:19:01 -0500

    clean up wireguard

--- machines/cardinal.nix
+++ machines/cardinal.nix
@@ -9,14 +9,7 @@
   };
 
   networking.hostName = "unallocatedspace";
-  services.openssh.settings.PasswordAuthentication = false;
-
-  networking.firewall = {
-      allowedUDPPorts = [
-        51820
-        51821
-      ];
-    };
+  services.unallocatedspace.enable = true;
 
   boot.kernel.sysctl."net.ipv6.conf.luna.ip_forward" = 1;
   boot.kernel.sysctl."net.ipv4.conf.luna.ip_forward" = 1;
@@ -25,77 +18,38 @@
   networking.nat.externalInterface = "ens3";
   networking.nat.internalInterfaces = [ "luni" ];
 
-
+  networking.firewall.allowedUDPPorts = [51820];
   # fd01:8cae:d246:5f03:XXXX:XXXX:XXXX:XXXX
   #  16    32   48   64   80   96  112  128
   networking.wireguard.interfaces.luni =
     {
       postSetup = ''
-         ${pkgs.iptables}/bin/ip6tables -A FORWARD -i luni -j ACCEPT
-         ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -o luni6 -j MASQUERADE
-
-
-        ${pkgs.iptables}/bin/iptables -A FORWARD -i luni -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -o luni -j MASQUERADE
+          ${pkgs.iptables}/bin/ip6tables -A FORWARD -i luni -j ACCEPT
+          ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -o luni -j MASQUERADE
+          ${pkgs.iptables}/bin/iptables -A FORWARD -i luni -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -o luni -j MASQUERADE
       '';
-
       postShutdown = ''
-         ${pkgs.iptables}/bin/ip6tables -D FORWARD -i luni6 -j ACCEPT
-         ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -o luni -j MASQUERADE
-
-         ${pkgs.iptables}/bin/iptables -D FORWARD -i luni4 -j ACCEPT
-         ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -o luni -j MASQUERADE
+          ${pkgs.iptables}/bin/ip6tables -D FORWARD -i luni -j ACCEPT
+          ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -o luni -j MASQUERADE
+          ${pkgs.iptables}/bin/iptables -D FORWARD -i luni -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -o luni -j MASQUERADE
       '';
-
       peers = with peers; (users ++ gateways);
       ips = ["fd01:1:a1::ff00/48" "10.51.0.128/24"];
       listenPort = 51820;
       privateKeyFile = "/var/lib/wireguard/privatekey";
     };
 
-  # fd01:8cae:d246:5f03:XXXX:XXXX:XXXX:XXXX
-  #  16    32   48   64   80   96  112  128
-  # networking.wireguard.interfaces.luni4 =
-  #   {
-  #     # postSetup = ''
-  #     #    ${pkgs.iptables}/bin/iptables -A FORWARD -i luni4 -j ACCEPT
-  #     #    ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -o luni4 -j MASQUERADE
-  #     # '';
-
-  #     # postShutdown = ''
-  #     #    ${pkgs.iptables}/bin/iptables -D FORWARD -i luni4 -j ACCEPT
-  #     #    ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -o luni4 -j MASQUERADE
-  #     # '';
-
-  #     peers = with peers; prot-ip ipv4 (users ++ gateways);
-  #     ips = ["10.51.0.255/24"];
-  #     listenPort = 51821;
-  #     privateKeyFile = "/var/lib/wireguard/ipv4-privatekey";
-  #   };
-
   services.coggiebot.enable = true;
   services.coggiebot.environmentFile = "/var/lib/coggiebot/env.sh";
 
-  boot.kernel.sysctl."vm.swappiness" = 30;
-
+  boot.kernel.sysctl."vm.swappiness" = 80;
   services.fail2ban.enable = true;
   services.fail2ban.bantime = "20m";
   services.fail2ban.bantime-increment.enable = true;
-  services.openssh.logLevel = "VERBOSE";
-
-    # networking.postUpCommands =
-    #   ''
-    #   ip6tables -t nat -A PREROUTING -p udp --dport 16262 -j DNAT --to-destination :16262
-    #   ip6tables -t nat -A POSTROUTING -p udp -d fd01:1:a1:1::2 --dport 16262 -j SNAT --to-source 192.168.12.87
-
-    #   ip6tables -t nat -A PREROUTING -p udp --dport 16261 -j DNAT --to-destination [fd01:1:a1:1::2]:16262
-    #   ip6tables -t nat -A POSTROUTING -p udp -d fd01:1:a1:1::2 --dport 16261 -j SNAT --to-source 192.168.12.87
-
-    #   ip6tables -t nat -A PREROUTING -p tcp --dport 2232 -j DNAT --to-destination [fd01:1:a1:1::2]:22
-    #   '';
-
-
-
+  services.openssh.settings.LogLevel = "VERBOSE";
+  services.openssh.settings.PasswordAuthentication = false;
 
   # services.journald.extraConfig = ''
   #   [Upload]
