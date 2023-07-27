af0b664b115fd13283ce07a5261bcc9ead3a4090
Skarlett <Skarlett@github.com>
2023-07-27 05:23:34 -0500

    try using unfree on all

--- extra-pkgs.nix
+++ extra-pkgs.nix
@@ -1,16 +1,17 @@
 {self, inputs, config, pkgs, lib, ...}:
 let cfg = {
-  inherit (pkgs.stdenv.hostPlatform) system;
-    config.allowUnfree = true;
+      inherit (pkgs.stdenv.hostPlatform) system;
+      config.allowUnfree = true;
   };
 in
 {
   # add all inputs to registry
   nix.registry = builtins.mapAttrs (k: v: { flake = v; }) inputs;
+  nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = with inputs; [
       (final: prev: {
         unstable = import nixpkgs-unstable cfg;
-        raccoon = import raccoon cfg; 
+        raccoon = import raccoon cfg;
       })
       (final: prev: {
         self = self.outputs.packages.${pkgs.stdenv.hostPlatform.system};
