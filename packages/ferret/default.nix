4e53564745a27233160eee26df1b9cd687b1852d
Skarlett <Skarlett@github.com>
2023-08-01 20:43:24 -0500

    fix: packages.ferret

--- packages/ferret/default.nix
+++ packages/ferret/default.nix
@@ -1,12 +1,20 @@
-{ stdenv, fetchFromGitHub, go, antlr }:
-
-stdenv.mkDerivation {
+{ stdenv, fetchFromGitHub, go, musl, antlr, buildGoModule }:
+buildGoModule {
   name = "ferret";
   src = fetchFromGitHub {
     owner = "MontFerret";
     repo = "ferret";
     rev = "v0.18.0";
-    hash = "xxx";
+    hash = "sha256-nFnmZ2Bo5B6N5nQW9xe/piUuhyEhFRQ9XotTMKe0Hrk=";
   };
-  buildInputs = [ go antlr ];
+  patches = [ ./rm-google-test.patch ];
+  vendorSha256 = "sha256-q7UGtYj0oLKK9UcjfUFqMLE/ZrJ8aruRtUnVmnJZMM0=";
+  buildInputs = [ stdenv go antlr ];
+  nativeBuildInputs = [musl];
+  CGO_ENABLED = 0;
+
+  ldflags = [
+    "-linkmode external"
+    "-extldflags '-static -L${musl}/lib'"
+  ];
 }
