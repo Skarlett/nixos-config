394b572860169ee5e330c7523f89bc8384d55480
Skarlett <Skarlett@github.com>
2023-08-19 11:52:17 -0500

    build from source

--- machines/live.nix
+++ machines/live.nix
@@ -3,7 +3,13 @@
   networking.hostName = "liveiso";
   services.openssh.enable = true;
   system.stateVersion = "23.05";
+
+  remote-access.lunarix = true;
+  common.enable = true;
+
   services.lvm.enable = false;
+  nix.settings.substituters = lib.mkForce [ ];
+  nix.settings.trusted-public-keys = lib.mkForce [ ];
 
   environment.systemPackages = [ pkgs.nixos-install-tools ];
 }
